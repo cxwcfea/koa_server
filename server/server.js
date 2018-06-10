e35b9a648ec7ac648c735ce13e27789a959a200f
cxwcfea <cxwcfea@163.com>
2018-06-10 17:33:46 +0800

    auto load route

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
-const Koa = require('koa');
-const Router = require('koa-router');
-const compress = require('koa-compress');
-const bodyParser = require('koa-bodyparser');
-const cors = require('kcors');
 const { scopePerRequest } = require('awilix-koa');
+const cors = require('kcors');
+const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const compress = require('koa-compress');
+const Router = require('koa-router');
+const glob = require('glob');
+const path = require('path');
 
 const { REQUEST_ID_HEADER } = require('./constants');
 const diContainer = require('./utils/di');
@@ -11,8 +13,6 @@ const errorHandler = require('./middleware/errorHandler');
 const log = require('./middleware/log');
 const logger = require('./utils/logger');
 const requestId = require('./middleware/requestId');
-
-const orders = require('./components/orders/order.route');
 
 const app = new Koa();
 const router = new Router();
@@ -30,7 +30,11 @@ app.use(scopePerRequest(diContainer));
 app.use(errorHandler());
 app.use(log({ logger }));
 
-orders(app);
+const routeFiles = glob.sync(path.join(__dirname, 'components', '**/*.route.js'), {});
+routeFiles.forEach((file) => {
+  // require('./components/orders/order.route')(app);
+  require(file)(app); // eslint-disable-line global-require
+});
 
 router.get('/test', (ctx) => {
   // ctx.body = 'Hello Koa';
